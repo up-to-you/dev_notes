7d73ceccc352bbc7f66d869086f6005971e08e62
up-to-you <artursabirovdev@gmail.com>
2017-11-21 20:37:48 +0300

    Create into_blob_from_cli.sql

---
+++ sql_hints/into_blob_from_cli.sql
@@ -0,0 +1,22 @@
+DECLARE
+--   you can't declare large string literals in standard sql
+--   hence you should use pl/sql
+  str VARCHAR2(32767);
+
+--   reference to table column datatype
+  some_raw_data SOME_TABLE.BLOB_COLUMN%TYPE;
+
+BEGIN
+  
+--   blob func requires raw (binary) data
+  some_raw_data:= utl_raw.cast_to_raw('LONG_STRING');
+
+  INSERT ALL
+    INTO SOME_TABLE VALUES (blobVal)
+    INTO NEXT_TABLE VALUES (blobVal)
+  SELECT
+    TO_BLOB(some_raw_data) blobVal
+  FROM dual;
+  COMMIT ;
+
+END;
